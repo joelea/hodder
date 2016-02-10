56f7ea069e72143cdd45f996b378f1d58be6a773
Joe Lea <joeleapublic@gmail.com>
2016-02-10 22:19:53 +0000

    Split out incrementer!

--- models/src/main/java/hodder/db/DataSourceFactory.java
+++ models/src/main/java/hodder/db/DataSourceFactory.java
@@ -1,23 +1,10 @@
 package hodder.db;
 
-import org.skife.jdbi.v2.DBI;
+import org.postgresql.jdbc3.Jdbc3PoolingDataSource;
 
 import javax.sql.DataSource;
-import java.util.function.Supplier;
 
 public class DataSourceFactory {
-
-    private static <T> T withRetry(Supplier<T> supplier) throws InterruptedException {
-        for(int i = 0; i < 60; i++) {
-            try {
-                return supplier.get();
-            } catch(Exception e) {
-                System.out.println("Failed to obtain connection to database. Retrying.");
-                Thread.sleep(1000);
-            }
-        }
-        throw new IllegalStateException("Failed to connect to the database after 60 attempts");
-    }
 
     public static DataSource create() {
         Jdbc3PoolingDataSource source = new Jdbc3PoolingDataSource();
