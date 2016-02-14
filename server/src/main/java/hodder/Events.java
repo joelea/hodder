cb30a57b32ef6110266da7e1af4c04f46268c096
Joe Lea <joeleapublic@gmail.com>
2016-02-14 16:42:59 +0000

    Move producer into properties file

--- server/src/main/java/hodder/Events.java
+++ server/src/main/java/hodder/Events.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class Events {
@@ -13,17 +14,18 @@ public class Events {
 
     public Events(String topic) {
         this.topic = topic;
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "kafka:9092");
-        props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
-        props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
+        Properties props = propertiesFromFile();
         producer = new KafkaProducer(props);
+    }
+
+    private Properties propertiesFromFile() {
+        try {
+            Properties props = new Properties();
+            props.load(this.getClass().getResourceAsStream("/kafka-producer.properties"));
+            return props;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean write(String message) {
