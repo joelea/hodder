503251419fd839404d7f05df4bb8742151e0e853
Joe Lea <joeleapublic@gmail.com>
2016-02-15 16:00:20 +0000

    Use new Delegating handler

--- todo-consumer/src/main/java/hodder/TodoConsumer.java
+++ todo-consumer/src/main/java/hodder/TodoConsumer.java
@@ -1,20 +1,16 @@
 package hodder;
 
-import com.google.common.collect.ImmutableMap;
 import hodder.events.TodoAdded;
 import kafka.Kafka;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.util.Map;
-
-import static hodder.Todos.*;
+import static hodder.Todos.createDefault;
 
 public class TodoConsumer {
     private static final String TOPIC = "hodder.todos";
     private static final Todos todos = createDefault();
-    private static final Map<Class, EventHandler> handlers = ImmutableMap.of(
-            TodoAdded.class, (todoAdded) -> handleTodoAdded(todoAdded)
-    );
+    private static final EventHandler handler = DelegatingEventHandler.create()
+        .delegate(TodoAdded.class, TodoConsumer::handleTodoAdded);
 
     private static void handleTodoAdded(TodoAdded todoAdded) {
         todos.addTodo(todoAdded.todoContents());
@@ -27,16 +23,9 @@ public class TodoConsumer {
 
         kafka.eventsIn(TOPIC)
                 .map(ConsumerRecord::value)
-                .subscribe( msg -> {
-                    handleEvent(todos, msg);
-                });
+                .subscribe(handler::handle);
 
         System.out.println("Hodder is flowing strong!");
-    }
-
-    private static void handleEvent(Todos todos, Event event) {
-        TodoAdded todoAdded = (TodoAdded) event;
-        todos.addTodo(todoAdded.todoContents());
     }
 
 }
